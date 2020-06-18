87d11f58c4e3dbb70720e76c7c373f48e3921026
wildcard <kay.neuenhofen@gmail.com>
2020-06-18 14:31:32 -0700

    initial checkin after fork: - added script to setup an agency based on agency ID, static gtfs feed and vehicle postion feed - tweaked hibernate connection url - running three cores: halifax, monterey and riverside:   + live map including unassigned vehicles shows plausible vehicle locations and movement vectors   + stop based predictions for a few stops for Halifax and Riverside show values that seem high but could be plausible

---
+++ setup-agency.sh
@@ -0,0 +1,77 @@
+#! /bin/sh
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: setup-agency <agency-id> <gtfs-url> <vehicle-positions-url>" >&2
+  exit 1
+fi
+
+AGENCY_ID=$1
+GTFS_URL=$2
+VEHICLE_URL=$3
+
+echo AGENCY_ID=$AGENCY_ID
+echo GTFS_URL=$GTFS_URL
+echo VEHICLE_URL=$VEHICLE_URL
+echo
+
+PROPFILE=${AGENCY_ID}.properties
+DBNAME=agency-$AGENCY_ID
+
+rm -f $PROPFILE
+touch $PROPFILE
+
+echo transitclock.core.agencyId=$AGENCY_ID >> $PROPFILE
+echo transitclock.db.dbUserName=wildcard >> $PROPFILE
+echo transitclock.db.dbPassword= >> $PROPFILE
+echo transitclock.db.dbName=$DBNAME >> $PROPFILE
+echo transitclock.db.dbHost=127.0.0.1 >> $PROPFILE
+echo transitclock.db.dbType=postgresql >> $PROPFILE
+echo transitclock.avl.gtfsRealtimeFeedURI=$VEHICLE_URL >> $PROPFILE
+echo transitclock.hibernate.connection.url=jdbc:postgresql://127.0.0.1:5432/$DBNAME >> $PROPFILE
+echo transitclock.hibernate.configFile=hibernate.cfg.xml >> $PROPFILE
+echo transitclock.modules.optionalModulesList=org.transitclock.avl.GtfsRealtimeModule >> $PROPFILE
+echo transitclock.web.mapTileUrl=https://tile.openstreetmap.org/{z}/{x}/{y}.png >> $PROPFILE
+echo transitclock.web.mapTileCopyright=OpenStreetMap >> $PROPFILE
+echo transitclock.rmi.secondaryRmiPort=0 >> $PROPFILE
+
+dropdb $DBNAME
+createdb $DBNAME || exit 0
+
+psql -d $DBNAME -f /Users/wildcard/projects/transitime/transitclock/src/main/resources/ddl_postgres_org_transitime_db_structs.sql || exit 0
+psql -d $DBNAME -f /Users/wildcard/projects/transitime/transitclock/src/main/resources/ddl_postgres_org_transitime_db_webstructs.sql || exit 0
+
+psql -P pager=off -q -d $DBNAME --command=\\dt
+
+java \
+  -Dtransitclock.logging.dir=logs \
+  -cp transitclock/target/Core.jar org.transitclock.applications.GtfsFileProcessor \
+  -c "$AGENCY_ID.properties" \
+  -storeNewRevs \
+  -skipDeleteRevs \
+  -gtfsUrl $GTFS_URL \
+  -maxTravelTimeSegmentLength 100 || exit 0
+
+java \
+  -cp transitclock/target/Core.jar org.transitclock.applications.CreateAPIKey \
+  -c "$AGENCY_ID.properties" \
+  -n "Buzz Killington" \
+  -u "https://www.google.com" \
+  -e "info@example.com" \
+  -p "4155550123" \
+  -d "Core access application" || exit 0
+
+java \
+  -Dhibernate.connection.url=jdbc:postgresql://127.0.0.1:5432/agency-halifax \
+  -Dhibernate.connection.username=wildcard \
+  -Dhibernate.connection.password= \
+  -Dtransitclock.hibernate.configFile=hibernate.cfg.xml \
+  -cp transitclock/target/Core.jar org.transitclock.db.webstructs.WebAgency \
+  $AGENCY_ID \
+  127.0.0.1 \
+  $DBNAME \
+  postgresql \
+  0.0.0.0 \
+  wildcard \
+  "" || exit 0
+
+
